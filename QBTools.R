3c95affb68817468f7389fc7b9d3c9ba93c5de36
ldurden <ldurden@indiana.edu>
2015-03-06 11:14:43 -0500

    Initial commit

---
+++ QBTools.R
@@ -0,0 +1,25 @@
+###############################################################################
+#                                                                             #
+#Quantitative Biodiversity Functions Source Code                              # 
+#                                                                             #         
+#                                                                             # 
+# Written by Lekeah and QB team Mario, Ken, and Jay                           #
+#                                                                             #
+#Last update: 2015/March                                                      #
+#                                                                             #
+# Notes : This file contains functions to calculate metrics of                #
+#         taxonomic and phylogenetic diversity                                # 
+#                                                                             #
+#                                                                             #
+###############################################################################
+
+install.packages("vegan")
+require("vegan")
+
+# My_function: returns estimate of beta biodiversity
+#Inputs: x, y, z
+
+My_function <- function(x,y,z){
+  ...
+}
+
